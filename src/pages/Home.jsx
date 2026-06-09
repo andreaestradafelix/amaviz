b0d6997f710626f8918903ca8d04d9d46092a30f
Andrea Estrada Felix <andreaestradafelix@gmail.com>
2026-06-09 14:35:33 -0700

    Hero and welcome

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,10 +6,17 @@ export default function Home() {
 
     return (
         <>
-        <Hero />
-            <div id="home">
+            <section className="amaviz-home" id="home">
+                <Hero />
 
-            </div>
+                <div className="amaviz-home-text">
+                    <p className="amaviz-welcome-text">Bienvenidos a Amavíz</p>
+                    <p className="amaviz-slogan-text">LA SALUD DE TU PIEL ES NUESTRA PRIORIDAD</p>
+                    <p className="amaviz-paragraph-text">En nuestro spa encontrarás un espacio diseñado para tu bienestar, donde
+                        cada tratatamiento está pensado para ayudarte a relajarte, renovar tu energía y cuidar tu piel en un ambiente de trabquilidad y armonía.</p>
+                </div>
+
+            </section>
         </>
     );
 
