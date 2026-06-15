8f90dfe35e555b23f3e9dd370aacb38f84451e1f
Andrea Estrada Felix <andreaestradafelix@gmail.com>
2026-06-15 10:03:29 -0700

    Creación de menú

---
+++ src/data/services.js
@@ -0,0 +1,14 @@
+
+export const services = [
+    {id: 1, type:"Faciales",            img:"/images/home/facial-photo.jpg"},
+    {id: 2, type:"Corporal",            img:"/images/home/masaje-photo.jpg"},
+    {id: 3, type:"Depilación Láser",    img:"/images/home/laser2-photo.jpg"},
+    {id: 4, type:"Más Servicios",       img:"/images/home/nails-photo.jpg"}
+]
+
+export const faciales = [
+    {id: 1, type:"Limpieza Profunda",            img:""},
+    {id: 2, type:"Corporal",            img:""},
+    {id: 3, type:"Depilación Láser",    img:""},
+    {id: 4, type:"Más Servicios",       img:""}
+]
---
+++ src/pages/Services/Services.jsx
@@ -0,0 +1,16 @@
+
+export default function Services(){
+    
+    <section className="amaviz-services" id="services">
+        
+        <h1 className="services-title">Faciales</h1>
+        <p className="services-text">Cada facial es personalizado según el tió de piel y necesidades</p>
+
+        <div className="services-list">
+
+        </div>
+
+
+    </section>
+
+}
